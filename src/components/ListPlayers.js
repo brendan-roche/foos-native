8d641342ca49ef30becaa97f4bafebf81c37acab
Brendan Roche <brendan@idealmedia.com.au>
2018-12-16 00:19:38 +1100

    Show summary of players in RecycledListView

--- src/components/ListPlayers.js
+++ src/components/ListPlayers.js
@@ -1,48 +1,217 @@
 // @flow
 import React, {Component} from 'react';
-import {View, Text, FlatList, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
 import {connect} from 'react-redux';
+import {Big} from 'big.js';
+import { RecyclerListView, DataProvider, LayoutProvider } from 'recyclerlistview';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import type {RootStore} from '../reducers';
 import type {IPlayer} from '../reducers/playerReducer';
 
 type Props = {
-    players: () => IPlayer[]
+    players: IPlayer[]
 }
 
-class ListPlayers extends Component<Props> {
-    renderItem = ({item}) => (
-        <View style={styles.item}>
-            <Text>{item.name}</Text>
-        </View>
-    );
+type State = {
+    dataProvider: DataProvider,
+    sortCol: string,
+    sortAsc: boolean,
+}
+
+const ViewTypes = {
+    ODD: 1,
+    EVEN: 2,
+};
+
+type DimensionsType = {
+    width: number,
+    height: number
+}
+
+class ListPlayers extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        let { width } = Dimensions.get('window');
+
+        // Create the data provider and provide method which takes in two rows of data
+        // and return if those two are different or not.
+        // THIS IS VERY IMPORTANT, FORGET PERFORMANCE IF THIS IS MESSED UP
+        let dataProvider = new DataProvider((r1, r2) => {
+            return r1 !== r2;
+        });
+
+        this._layoutProvider = new LayoutProvider(
+            (index: number): number => {
+                return index % 2 === 0
+                    ? ViewTypes.EVEN
+                    : ViewTypes.ODD;
+            },
+            (type: number, dim: DimensionsType) => {
+                dim.width = width;
+                dim.height = 35;
+            }
+        );
+
+        // Since component should always render once data has changed, make data provider part of the state
+        this.state = {
+            dataProvider: dataProvider.cloneWithRows(this.props.players),
+            sortCol: 'name',
+            sortAsc: true,
+        };
+    }
+
+    orderBy(column: string) {
+        return () => {
+
+            const {sortAsc, sortCol, dataProvider} = this.state;
+            const ascending = column === sortCol
+                ? !sortAsc
+                : true;
+            const players = this.props.players.sort(
+                comparePlayers.bind(this, column, ascending)
+            );
+
+            this.setState({
+                dataProvider: dataProvider.cloneWithRows(players),
+                sortCol: column,
+                sortAsc: ascending,
+            });
+        }
+    }
+
+    // Given type and data return the view component
+    _rowRenderer = (type: number, player: IPlayer) => {
+        return (
+            <>
+                <TouchableOpacity
+                    style={type === ViewTypes.EVEN ? styles.rowEven : styles.rowOdd}
+                    onPress={() => console.log('onPress')}
+                >
+                    <Text style={styles.nameCol}>{player.name}</Text>
+                    <Text style={styles.winsCol}>{player.wins}</Text>
+                    <Text style={styles.lossesCol}>{player.losses}</Text>
+                    <Text style={styles.trueSkillCol}>{Big(player.trueskill).toFixed(2)}</Text>
+                    <Text style={styles.trueSkillSigmaCol}>{Big(player.trueskillSigma).toFixed(2)}</Text>
+                </TouchableOpacity>
+            </>
+        );
+    };
 
     render() {
-        const {players} = this.props;
-        return (
-            <FlatList
-                styles={styles.container}
-                data={players}
-                renderItem={this.renderItem}
+        const {sortCol, sortAsc} = this.state;
+        const icon = <Icon name={sortAsc ? 'arrow-down' : 'arrow-up'} size={10} />;
+
+        return <>
+            <View style={styles.rowHeader}>
+                <Text
+                    onPress={this.orderBy('name')}
+                    style={styles.nameHeader}
+                >Name {sortCol === 'name' ? icon : null}</Text>
+                <Text
+                    onPress={this.orderBy('wins')}
+                    style={styles.winsHeader}
+                >Wins {sortCol === 'wins' ? icon : null}</Text>
+                <Text
+                    onPress={this.orderBy('losses')}
+                    style={styles.lossesHeader}
+                >Losses {sortCol === 'losses' ? icon : null}</Text>
+                <Text
+                    onPress={this.orderBy('trueskill')}
+                    style={styles.trueSkillHeader}
+                >TrueSkill {sortCol === 'trueskill' ? icon : null}</Text>
+                <Text
+                    onPress={this.orderBy('trueskillSigma')}
+                    style={styles.trueSkillSigmaHeader}
+                >TrueSkill σ {sortCol === 'trueskillSigma' ? icon : null}</Text>
+            </View>
+            <RecyclerListView
+                layoutProvider={this._layoutProvider}
+                dataProvider={this.state.dataProvider}
+                rowRenderer={this._rowRenderer}
             />
-        );
+        </>;
     }
+}
+
+function comparePlayers(sortCol: string | number, ascending: boolean, p1: IPlayer, p2: IPlayer): number {
+    if ((p1[sortCol] < p2[sortCol] && ascending) || (p1[sortCol] > p2[sortCol] && !ascending)) {
+        return -1;
+    }
+    if ((p1[sortCol] > p2[sortCol] && ascending) || (p1[sortCol] < p2[sortCol] && !ascending)) {
+        return 1;
+    }
+
+    return 0;
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
+    rowHeader: {
+        justifyContent: "space-around",
+        alignItems: "center",
+        flexDirection: 'row',
+        backgroundColor: '#ffbb00',
+        height: 40,
     },
-    item: {
-        padding: 16,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc'
-    }
+    rowEven: {
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#00BFFF',
+    },
+    rowOdd: {
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#ffffff',
+    },
+    nameHeader: {
+        marginLeft: 5,
+        flex: 1.4,
+        fontWeight: 'bold',
+    },
+    winsHeader: {
+        flex: 0.7,
+        fontWeight: 'bold',
+    },
+    lossesHeader: {
+        flex: 0.9,
+        fontWeight: 'bold',
+    },
+    trueSkillHeader: {
+        flex: 1.2,
+        fontWeight: 'bold',
+    },
+    trueSkillSigmaHeader: {
+        flex: 1.3,
+        fontWeight: 'bold',
+    },
+    nameCol: {
+        marginLeft: 5,
+        flex: 1.4
+    },
+    winsCol: {
+        flex: 0.7
+    },
+    lossesCol: {
+        flex: 0.9
+    },
+    trueSkillCol: {
+        flex: 1.2
+    },
+    trueSkillSigmaCol: {
+        flex: 1.3
+    },
 });
 
 const mapStateToProps = (state: RootStore) => {
     return {
-        players: Array.from(state.players.players.values())
+        players: Array.from(
+            state.players.players.values()
+        ).filter((player: IPlayer) => player.active)
     };
 };
 
