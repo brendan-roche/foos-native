bf631ea6f98f87f692b0091fa39b6d18a0a7d0d2
Brendan Roche <brendan@idealmedia.com.au>
2019-01-28 16:16:31 +1100

    Use different base url based on whether dev build and an emulator

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,17 @@
+import { Platform } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
+
+let apiBaseUrl;
+
+// eslint-disable-next-line
+if (__DEV__) {
+  apiBaseUrl =
+    Platform.OS === 'android' && DeviceInfo.isEmulator()
+      ? 'http://10.0.2.2:5000'
+      : 'http://localhost:5000';
+} else {
+  apiBaseUrl = 'http://foos.test.kounta.com';
+}
+
 // eslint-disable-next-line import/prefer-default-export
-export const API_BASE_URL = 'http://foos.test.kounta.com';
-// export const API_BASE_URL = 'http://localhost:5000';
-// export const API_BASE_URL = 'http://10.0.2.2:5000';
+export const API_BASE_URL = apiBaseUrl;
